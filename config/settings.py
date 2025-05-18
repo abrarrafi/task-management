d1cc9a01b1458bec9dcc13b1d7ee810e835ba0a2
Abrar Shahriar <abrarrafi174@gmail.com>
2025-05-18 20:25:47 +0600

    Initial commit

--- config/settings.py
+++ config/settings.py
@@ -46,10 +46,10 @@ INSTALLED_APPS = [
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'silentfacts981@gmail.com'
+EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = 'ekkr mylc oykg qhxr'
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'silentfacts981@gmail.com'
+DEFAULT_FROM_EMAIL = ''
 
 
 MIDDLEWARE = [
